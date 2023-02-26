b17a65721c15d95b1f789ab456f5b8d101b58269
kcmki <mekkimohe@gmail.com>
2023-02-26 17:47:19 +0100

    update

--- S2/metaH/Dame/src/application/Main.java
+++ S2/metaH/Dame/src/application/Main.java
@@ -1,20 +1,28 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			
+			Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
+			primaryStage.setScene(new Scene(root));
+			
+			Image icon = new Image("crown.png");
+			primaryStage.setResizable(false);
+			primaryStage.getIcons().add(icon);
+			primaryStage.setTitle("Dame");
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
